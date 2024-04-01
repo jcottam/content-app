12056415cc3fd90dfa2e96c9a34d1077d32af8e6
John Ryan Cottam <jcottam@gmail.com>
2024-04-01 15:33:49 -0400

    added theme

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,10 @@
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+  <ContentDoc class="content-post">
+    <template #not-found>
+      <h1>Document not found</h1>
+    </template>
+    <template #empty>
+      <h1>Document is empty</h1>
+    </template>
+  </ContentDoc>
 </template>
